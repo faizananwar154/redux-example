99bea0ba8eab446f316c466a8fd725222a92aecd
faizananwar154 <faizan.anwar@tiksom.com>
2020-10-13 15:17:51 +0500

    - Adds middleware to create logs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combinedReducers = redux.combineReducers;
+const logger = reduxLogger.createLogger();
+const applyMiddleware = redux.applyMiddleware;
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICE_CREAM = 'BUY_ICE_CREAM';
@@ -52,9 +56,9 @@ const rootReducer = combinedReducers({
   iceCream: iceCreamReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('initial state is', store.getState());
-const unsubscribe = store.subscribe(() => console.log('current state is', store.getState()));
+const unsubscribe = store.subscribe(() => { } );
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 store.dispatch(buyCake());
